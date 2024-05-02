0ee7733e254c13902e84fdb7b1b7895678de439b
egorMiwa2 <egorMiwa2@gmail.com>
2024-05-02 21:37:03 +0300

    yahz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { ExampleTable } from "./components/ExampleTable/Table";
 import { ExampleFormDataProvider } from "./provider/DataProvider";
+import { NoSSR } from "next-dynamic-no-ssr";
+
 import { useGridApiContext } from "@mui/x-data-grid";
 
 export default function Home() {
   return (
+    <NoSSR>
     <ExampleFormDataProvider>
       <main className={styles.main}>
         <Sidebar />
         <ExampleTable />
       </main>
     </ExampleFormDataProvider>
+    </NoSSR>
   );
 }
+
